b3c81fab5ae95848c89f2137dc47c2132675ec9c
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-07-30 10:10:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Custom Fail Text
+
+Allows you to customize the text that appears when you fail a level.
+
+## Credits
+
+* [zoller27osu](https://github.com/zoller27osu), [Sc2ad](https://github.com/Sc2ad) and [jakibaki](https://github.com/jakibaki) - [beatsaber-hook](https://github.com/sc2ad/beatsaber-hook)
+* [raftario](https://github.com/raftario) - [vscode-bsqm](https://github.com/raftario/vscode-bsqm) and [this template](https://github.com/raftario/bmbf-mod-template)
---
+++ buildBMBF.ps1
@@ -0,0 +1,11 @@
+# Builds a .zip file for loading with BMBF
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $buildScript += ".cmd"
+}
+
+& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+Remove-Item .\custom-fail-text_v0.1.0.zip
+Compress-Archive -Path "./libs/arm64-v8a/libcustom-fail-text.so","./bmbfmod.json","./include/libs/libbeatsaber-hook_0_2_1.so" -DestinationPath "./custom-fail-text_v0.1.0.zip" -Update
