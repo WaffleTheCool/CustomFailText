6c1a9d3222e83528252d8f76b392e6893a6aa364
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-08-19 08:15:01 +0100

    BS hook 0.4.3

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -7,5 +7,5 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Remove-Item .\custom-fail-text_v0.1.1.zip
-Compress-Archive -Path "./libs/arm64-v8a/libcustom-fail-text.so","./bmbfmod.json","./include/libs/libbeatsaber-hook_0_2_1.so" -DestinationPath "./custom-fail-text_v0.1.1.zip" -Update
+Remove-Item .\custom-fail-text_v0.1.2.zip
+Compress-Archive -Path "./libs/arm64-v8a/libcustom-fail-text.so","./bmbfmod.json","./include/libs/libbeatsaber-hook_2019_0_4_3.so" -DestinationPath "./custom-fail-text_v0.1.2.zip" -Update
