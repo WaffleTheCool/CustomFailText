c27c729229056e4fcce246a367c044a2a119468d
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-11-13 07:28:41 +0000

    Updated libraries for Beat Saber 1.13.0

--- Android.mk
+++ Android.mk
@@ -27,16 +27,16 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 # Build the beatsaber-hook shared library, SPECIFICALLY VERSIONED!
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_0_8_2
+LOCAL_MODULE := beatsaber-hook_0_8_4
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_8_2.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_8_4.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: codegen - version: 0.3.4
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_3_5
+LOCAL_MODULE := codegen_0_3_6
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_3_5.so
+LOCAL_SRC_FILES := extern/libcodegen_0_3_6.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: custom-types - version: 0.2.9
 include $(CLEAR_VARS)
@@ -52,17 +52,17 @@ LOCAL_SRC_FILES := extern/libquestui.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := custom-fail-text_0_1_6
+LOCAL_MODULE := custom-fail-text_0_1_7
 LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_8_2
-LOCAL_SHARED_LIBRARIES += codegen_0_3_5
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_8_4
+LOCAL_SHARED_LIBRARIES += codegen_0_3_6
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_LDLIBS += -llog
-LOCAL_CFLAGS += -I'./extern/libil2cpp/il2cpp/libil2cpp' -DID='"custom-fail-text"' -DVERSION='"0.1.6"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
+LOCAL_CFLAGS += -I'./extern/libil2cpp/il2cpp/libil2cpp' -DID='"custom-fail-text"' -DVERSION='"0.1.7"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
 LOCAL_CPPFLAGS += -std=c++2a
 LOCAL_C_INCLUDES += ./include ./src
 include $(BUILD_SHARED_LIBRARY)
--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -8,4 +8,4 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 
-Compress-Archive -Path "./libs/arm64-v8a/libcustom-fail-text_0_1_6.so", "./extern/libbeatsaber-hook_0_8_2.so", "./extern/libbeatsaber-hook_0_8_0.so", "./extern/libcodegen_0_3_5.so", "./extern/libcustom-types.so", "./extern/libquestui.so", "./bmbfmod.json" -DestinationPath "./custom-fail-text_v0.1.6.zip" -Update
+Compress-Archive -Path "./libs/arm64-v8a/libcustom-fail-text_0_1_7.so", "cover.png", "./extern/libbeatsaber-hook_0_8_4.so", "./extern/libcodegen_0_3_6.so", "./extern/libcustom-types.so", "./extern/libquestui.so", "./bmbfmod.json" -DestinationPath "./custom-fail-text_v0.1.7.zip" -Update
